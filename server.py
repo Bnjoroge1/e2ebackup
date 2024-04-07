6df2abdc45b9927819e9dc9c66516a7bbd7f87aa
Bill Njoroge <williamsriunge@gmail.com>
2024-04-07 01:17:59 -0400

    added login

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 from pydantic import BaseModel, EmailStr, SecretStr
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlmodels import Base, User, FileMetadata, SessionLocal, engine
 import jwt
 from sqlalchemy.orm import Session
+import secrets
+import os
+from datetime import datetime, timedelta
+
 
 
 
@@ -46,7 +51,37 @@ def sign_up(user_data: UserRegister, db: Session = Depends(get_db)):
 
      
 
+#Handcoding for now but should prolly also be stored in the HSM keyvault. 
+SECRET_KEY = "L39UIWMb1L2U2rCbtjcJSnHpqdHWo_BmxHpDWXLSew"
+ALGORITHM = "H256"
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+ #jwt authentication
+def create_access_token(data: dict, expires_delta: timedelta = None):
+     to_encode = data.copy()
+     expire = datetime.utcnow() + (expires_delta if expires_delta else timedelta(minutes=15))
+     to_encode.update({"exp": expire})
+     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+     return encoded_jwt
+
+def authenticate_user(email:str, password:str, db:Session):
+     user  = db.query(User).filter(User.email==email).first()
+     if not user:
+          return False
+     #verify password TODO using OPAQUE protocol.
+     if not verify_password(password, user.hashed_password):
+          return False
+     return user
+
+@app.post("/login")
+def login(form_data: OAuth2PasswordRequestForm= Depends(), db: Session = Depends(get_db)):
+     user = authenticate_user(form_data.username, form_data.password, db)
+     if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+     access_token = create_access_token(data={"sub": user.email})
+     return {"access_token": access_token, "token_type": "bearer"}
+
+          
 
 
 
---
+++ test.py
@@ -0,0 +1,4 @@
+
+import secrets
+SECRET_KEY = secrets.token_urlsafe(32)
+print(SECRET_KEY)
